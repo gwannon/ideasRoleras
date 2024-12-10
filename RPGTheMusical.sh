45e8f3e02d8d5375539744fe013135496a06a300
Gwannon <monclus.jorge@gmail.com>
2024-12-10 09:28:41 +0100

    Minor fix

--- RPGTheMusical.sh
+++ RPGTheMusical.sh
@@ -4,5 +4,5 @@ php ./RPGTheMusical/generateDocs.php
 chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./RPGTheMusical/temp.pdf ./RPGTheMusical/RPGTheMusical.html
 pdftk './RPGTheMusical/temp.pdf' update_info_utf8 './RPGTheMusical/metas.txt' output 'RPGTheMusical.pdf'
 rm ./RPGTheMusical/metas.txt
-#rm ./RPGTheMusical/RPGTheMusical.html
+rm ./RPGTheMusical/RPGTheMusical.html
 rm ./RPGTheMusical/temp.pdf
