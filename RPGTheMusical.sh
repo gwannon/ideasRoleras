4b752e5ba02ee30e726a014df730ad848e0937dc
Gwannon <monclus.jorge@gmail.com>
2024-12-11 11:01:37 +0100

    Reducimos proceso de creación PDF

--- RPGTheMusical.sh
+++ RPGTheMusical.sh
@@ -2,7 +2,6 @@
 
 php ./RPGTheMusical/generateDocs.php
 chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./RPGTheMusical/temp.pdf ./RPGTheMusical/RPGTheMusical.html
-pdftk './RPGTheMusical/temp.pdf' update_info_utf8 './RPGTheMusical/metas.txt' output 'RPGTheMusical.pdf'
-rm ./RPGTheMusical/metas.txt
+pdftk './RPGTheMusical/temp.pdf' update_info_utf8 './RPGTheMusical/metas.txt' output 'RPGTheMusical.pdf' compress
 rm ./RPGTheMusical/RPGTheMusical.html
 rm ./RPGTheMusical/temp.pdf
--- RPGTheMusical/generateDocs.php
+++ RPGTheMusical/generateDocs.php
@@ -7,30 +7,11 @@ require __DIR__ . '/../vendor/autoload.php';
 use FastVolt\Helper\Markdown;
 $mkd = Markdown::new();
 $mkd->setContent(file_get_contents(__DIR__ . "/../RPGTheMusical.md"));
-$tags['HTML'] = $mkd->toHtml();
-$html = file_get_contents(__DIR__ . "/template.html");
-foreach ($tags as $tag => $value) {
-  $html = str_replace("|".$tag."|", $value, $html); 
-}
+
+$html = str_replace("|HTML|", $mkd->toHtml(), file_get_contents(__DIR__ . "/template.html")); 
 $html = str_replace("<hr />", "</div><div class=\"saltopagina\"></div>\n</section>\n<section>", $html); 
 $html = str_replace("<p>\saltopagina</p>", "</div><div class=\"saltopagina\"></div><div class='columns'>", $html);
 $html = str_replace("<p>\saltocolumna</p>", "<p class=\"saltocolumna\"></p>", $html);
-//$html = str_replace("<p>\sincolumna</p>", "</div>", $html);
-//$html = str_replace("<p>\concolumna</p>", "<div class='columns'>", $html);
 $html = str_replace("<div class='columns'>\n</div>", "", $html); 
-//$html = str_replace("<table>", "<div><table>", $html); 
-//$html = str_replace("</table>", "</table></div>", $html); 
 
-/* Generamos Metas */
-/* -------------------------------------------------------------- */
-$metas = "InfoKey: Title\n";
-$metas .= "InfoValue: RPG The musical\n\n";
-$metas .= "InfoKey: Subject\n";
-$metas .= "InfoValue: RPG The musical (RpgTM) es una ayuda rolera en una sola hoja para jugar escenas musicales en tus partidas. Versión 1.0\n\n";
-$metas .= "InfoKey: Author\n";
-$metas .= "InfoValue: Gwannon\n\n";
-$metas .= "InfoKey: Keywords\n";
-$metas .= "InfoValue: rpg, musical, one-page\n\n";
-
-file_put_contents(__DIR__ . "/metas.txt", $metas);
 file_put_contents(__DIR__ . "/RPGTheMusical.html", $html);
