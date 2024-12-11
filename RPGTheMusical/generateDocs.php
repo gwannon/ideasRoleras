96badb58c59a596d3c99214bb63fbcf984d14a6d
Gwannon <monclus.jorge@gmail.com>
2024-12-11 10:42:46 +0100

    Mejora de peso

--- RPGTheMusical/generateDocs.php
+++ RPGTheMusical/generateDocs.php
@@ -15,11 +15,11 @@ foreach ($tags as $tag => $value) {
 $html = str_replace("<hr />", "</div><div class=\"saltopagina\"></div>\n</section>\n<section>", $html); 
 $html = str_replace("<p>\saltopagina</p>", "</div><div class=\"saltopagina\"></div><div class='columns'>", $html);
 $html = str_replace("<p>\saltocolumna</p>", "<p class=\"saltocolumna\"></p>", $html);
-$html = str_replace("<p>\sincolumna</p>", "</div>", $html);
-$html = str_replace("<p>\concolumna</p>", "<div class='columns'>", $html);
+//$html = str_replace("<p>\sincolumna</p>", "</div>", $html);
+//$html = str_replace("<p>\concolumna</p>", "<div class='columns'>", $html);
 $html = str_replace("<div class='columns'>\n</div>", "", $html); 
-$html = str_replace("<table>", "<div><table>", $html); 
-$html = str_replace("</table>", "</table></div>", $html); 
+//$html = str_replace("<table>", "<div><table>", $html); 
+//$html = str_replace("</table>", "</table></div>", $html); 
 
 /* Generamos Metas */
 /* -------------------------------------------------------------- */
