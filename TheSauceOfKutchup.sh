26172cf8852c5a66c100c551428a1754c29d07f0
Gwannon <monclus.jorge@gmail.com>
2024-12-21 23:04:33 +0100

    Guía del chef

--- TheSauceOfKutchup.sh
+++ TheSauceOfKutchup.sh
@@ -9,5 +9,5 @@ rm ./TheSauceOfKutchup/temp.pdf
 php ./TheSauceOfKutchup/generateDocs.php TheSauceOfKutchupGuiaDelChef
 chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./TheSauceOfKutchup/temp.pdf ./TheSauceOfKutchup/TheSauceOfKutchupGuiaDelChef.html
 pdftk './TheSauceOfKutchup/temp.pdf' update_info_utf8 './TheSauceOfKutchup/metas2.txt' output 'TheSauceOfKutchupGuiaDelChef.pdf' compress
-rm ./TheSauceOfKutchup/TheSauceOfKutchupGuiaDelChef.html
+#rm ./TheSauceOfKutchup/TheSauceOfKutchupGuiaDelChef.html
 rm ./TheSauceOfKutchup/temp.pdf
