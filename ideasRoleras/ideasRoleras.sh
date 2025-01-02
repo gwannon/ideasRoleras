968572ee39e72d323f30e37f9ec6dc93dd36c8fe
Gwannon <monclus.jorge@gmail.com>
2025-01-02 22:44:03 +0100

    Más ideas roleras

--- ideasRoleras/ideasRoleras.sh
+++ ideasRoleras/ideasRoleras.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cd ./ideasRoleras/
 php ./generateDocs.php
 chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./temp.pdf ./ideasRoleras.html
 pdftk './temp.pdf' update_info_utf8 './metas.txt' output '../ideasRoleras.pdf' compress
