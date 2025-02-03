e81b2fbad15a17604b50e920e0f83c26546c62c9
Gwannon <monclus.jorge@gmail.com>
2025-02-03 13:54:31 +0100

    GC: Sistema antiguo

--- GuiaDeCazaVampirosParaNinasMolonas/pdf.sh
+++ GuiaDeCazaVampirosParaNinasMolonas/pdf.sh
@@ -5,5 +5,5 @@ chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./t
 pdftk 'temp.pdf' update_info_utf8 'metas.txt' output 'GuiaDeCazaVampirosParaNinasMolonas.pdf'
 rm metas.txt
 #rm GuiaDeCazaVampirosParaNinasMolonas.html
-mv GuiaDeCazaVampirosParaNinasMolonas.html index.html
+#mv GuiaDeCazaVampirosParaNinasMolonas.html index.html
 rm temp.pdf
