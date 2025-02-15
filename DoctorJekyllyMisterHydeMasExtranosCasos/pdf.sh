824267fd526558528bf879d7962d4a465dcd7f4d
Gwannon <monclus.jorge@gmail.com>
2025-02-15 23:41:57 +0100

    Mejoras proceso creación pdf

--- DoctorJekyllyMisterHydeMasExtranosCasos/pdf.sh
+++ DoctorJekyllyMisterHydeMasExtranosCasos/pdf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 php ./tools/generateDocs.php
-chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./temp.pdf ./DoctorJekyllyMisterHydeMasExtranosCasos.html
+google-chrome --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./temp.pdf ./DoctorJekyllyMisterHydeMasExtranosCasos.html
 pdftk 'temp.pdf' update_info_utf8 'metas.txt' output 'DoctorJekyllyMisterHydeMasExtranosCasos.pdf'
 rm metas.txt
 #rm DoctorJekyllyMisterHydeMasExtranosCasos.html
