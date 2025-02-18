a36c962bf63a010fe9f5a6647ef7df76a62a2405
gwannon <monclus.jorge@gmail.com>
2025-02-18 15:47:52 +0100

    GC: Mejoras pdf.sh

--- GuiaDeCazaVampirosParaNinasMolonas/pdf.sh
+++ GuiaDeCazaVampirosParaNinasMolonas/pdf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 php ./tools/generateDocs.php
-chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./temp.pdf ./GuiaDeCazaVampirosParaNinasMolonas.html
+google-chrome --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./temp.pdf ./GuiaDeCazaVampirosParaNinasMolonas.html
 pdftk 'temp.pdf' update_info_utf8 'metas.txt' output 'GuiaDeCazaVampirosParaNinasMolonas.pdf'
 rm metas.txt
 #rm GuiaDeCazaVampirosParaNinasMolonas.html
