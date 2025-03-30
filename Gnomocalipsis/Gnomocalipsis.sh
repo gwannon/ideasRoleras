caf791b7c2200c6f153f98087cd817de2b366548
Gwannon <monclus.jorge@gmail.com>
2025-03-30 10:25:50 +0200

    Gnomocalipsis Diseño

--- Gnomocalipsis/Gnomocalipsis.sh
+++ Gnomocalipsis/Gnomocalipsis.sh
@@ -3,6 +3,6 @@
 php ./generateDocs.php Gnomocalipsis
 chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./temp.pdf ./Gnomocalipsis.html
 pdftk './temp.pdf' update_info_utf8 './Gnomocalipsis.txt' output '../Gnomocalipsis.pdf' compress
-#rm ./Gnomocalipsis.html
+rm ./Gnomocalipsis.html
 rm ./temp.pdf
 
