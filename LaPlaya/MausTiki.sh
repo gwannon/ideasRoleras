4b977e0c12feb3df8d1254a3ceb7d2bc71b92ba5
gwannon <monclus.jorge@gmail.com>
2025-04-23 11:01:31 +0200

    LP: Suplementos

--- LaPlaya/MausTiki.sh
+++ LaPlaya/MausTiki.sh
@@ -6,3 +6,11 @@ pdftk './temp.pdf' update_info_utf8 './MausTiki.txt' output '../MausTiki.pdf' co
 #rm ./MausTiki.html
 rm ./MausTiki.txt
 rm ./temp.pdf
+
+
+php ./generateDocs.php SuplementoMagiaTiki
+chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./temp.pdf ./SuplementoMagiaTiki.html
+pdftk './temp.pdf' update_info_utf8 './SuplementoMagiaTiki.txt' output '../SuplementoMagiaTiki.pdf' compress
+rm ./SuplementoMagiaTiki.html
+rm ./SuplementoMagiaTiki.txt
+rm ./temp.pdf
