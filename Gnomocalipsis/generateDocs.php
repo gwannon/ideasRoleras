958ab17572f15947efcf1d120ab8583439189049
Gwannon <monclus.jorge@gmail.com>
2025-07-25 11:33:09 +0200

    Lanzamiento Gnomocalipsis

--- Gnomocalipsis/generateDocs.php
+++ Gnomocalipsis/generateDocs.php
@@ -8,7 +8,7 @@ use FastVolt\Helper\Markdown;
 $mkd = Markdown::new();
 $mkd->setContent(file_get_contents(__DIR__ . "/".$argv[1].".md"));
 
-$html = str_replace("|HTML|", $mkd->toHtml(), file_get_contents(__DIR__ . "/template.html")); 
+$html = str_replace("|HTML|", $mkd->toHtml(), file_get_contents(__DIR__ . "/template".$argv[1].".html")); 
 $html = str_replace("|ID|", $argv[1], $html); 
 $html = str_replace("<hr />", "</div><div class=\"saltopagina\"></div>\n</section>\n<section>", $html); 
 $html = str_replace("<p>\sp</p>", "</div><div class=\"saltopagina\"></div><div class='columns'>", $html);
