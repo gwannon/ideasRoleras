3e25639916326a8e24afb850e73c856763a6b87c
Gwannon <monclus.jorge@gmail.com>
2025-07-26 11:45:08 +0200

    Lanzamiento Bandageless

--- Bandageless/pdf.sh
+++ Bandageless/pdf.sh
@@ -3,12 +3,12 @@
 php ./generateDocs.php BandagelessES
 chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./temp.pdf ./BandagelessES.html
 pdftk './temp.pdf' update_info_utf8 './BandagelessES.txt' output '../BandagelessES.pdf' compress
-rm ./BandagelessES.html
+#rm ./BandagelessES.html
 rm ./temp.pdf
 
 
 php ./generateDocs.php BandagelessEN
 chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./temp.pdf ./BandagelessEN.html
 pdftk './temp.pdf' update_info_utf8 './BandagelessEN.txt' output '../BandagelessEN.pdf' compress
-rm ./BandagelessEN.html
+#rm ./BandagelessEN.html
 rm ./temp.pdf
