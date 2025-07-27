e56218427910e9f0bae9fc242f1c7549949b4e0e
Gwannon <monclus.jorge@gmail.com>
2025-07-27 18:30:31 +0200

    arreglos varios publicación

---
+++ NotResponding/pdf.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+php ./generateDocs.php NotRespondingES
+chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./temp.pdf ./NotRespondingES.html
+pdftk './temp.pdf' update_info_utf8 './NotRespondingES.txt' output '../NotRespondingES.pdf' compress
+#rm ./NotRespondingES.html
+rm ./temp.pdf
+
+php ./generateDocs.php NotRespondingEN
+chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./temp.pdf ./NotRespondingEN.html
+pdftk './temp.pdf' update_info_utf8 './NotRespondingEN.txt' output '../NotRespondingEN.pdf' compress
+#rm ./NotRespondingEN.html
+rm ./temp.pdf
+
+#php ./generateDocs.php DesbordamientoPilaES
+#chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./DesbordamientoPilaES.pdf ./DesbordamientoPilaES.html
+#rm ./DesbordamientoPilaES.html
