36ffc7a7cfb065ebbdc08d3972dfbd30a3295221
Gwannon <monclus.jorge@gmail.com>
2025-07-27 02:23:49 +0200

    Lanzamiento NotResponding

--- NotResponding/NotResponding.sh
+++ NotResponding/NotResponding.sh
@@ -3,13 +3,13 @@
 php ./generateDocs.php NotRespondingES
 chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./temp.pdf ./NotRespondingES.html
 pdftk './temp.pdf' update_info_utf8 './NotRespondingES.txt' output '../NotRespondingES.pdf' compress
-rm ./NotRespondingES.html
+#rm ./NotRespondingES.html
 rm ./temp.pdf
 
 php ./generateDocs.php NotRespondingEN
 chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./temp.pdf ./NotRespondingEN.html
 pdftk './temp.pdf' update_info_utf8 './NotRespondingEN.txt' output '../NotRespondingEN.pdf' compress
-rm ./NotRespondingEN.html
+#rm ./NotRespondingEN.html
 rm ./temp.pdf
 
 #php ./generateDocs.php DesbordamientoPilaES
