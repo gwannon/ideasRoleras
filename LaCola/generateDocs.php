2f8b7527fec660ac130728d63c204893f4c4b52e
Gwannon <monclus.jorge@gmail.com>
2025-07-28 17:02:45 +0200

    Lanzamiento LaCola

--- LaCola/generateDocs.php
+++ LaCola/generateDocs.php
@@ -13,7 +13,7 @@ file_put_contents(__DIR__ . "/Acc".$argv[1].".md", str_replace(["\sp", "\sc", "\
 
 $mkd->setContent($md);
 
-$html = str_replace("|HTML|", $mkd->toHtml(), file_get_contents(__DIR__ . "/template.html")); 
+$html = str_replace("|HTML|", $mkd->toHtml(), file_get_contents(__DIR__ . "/template".$argv[1].".html")); 
 $html = str_replace("|ID|", $argv[1], $html); 
 $html = str_replace("<hr />", "</div><div class=\"saltopagina\"></div>\n</section>\n<section>", $html); 
 $html = str_replace("<p>\sp</p>", "</div><div class=\"saltopagina\"></div><div class='columns'>", $html);
