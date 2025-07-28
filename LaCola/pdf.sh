815bbd1a3a2be528b26e2b049df0651985ad5b56
Gwannon <monclus.jorge@gmail.com>
2025-07-28 16:33:58 +0200

    Lanzamiento LaCola

--- LaCola/pdf.sh
+++ LaCola/pdf.sh
@@ -3,11 +3,11 @@
 php ./generateDocs.php LaCola
 chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./temp.pdf ./LaCola.html
 pdftk './temp.pdf' update_info_utf8 './LaCola.txt' output '../LaCola.pdf' compress
-rm ./LaCola.html
+#rm ./LaCola.html
 rm ./temp.pdf
 
 php ./generateDocs.php TheQueue
 chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./temp.pdf ./TheQueue.html
 pdftk './temp.pdf' update_info_utf8 './TheQueue.txt' output '../TheQueue.pdf' compress
-rm ./TheQueue.html
+#rm ./TheQueue.html
 rm ./temp.pdf
