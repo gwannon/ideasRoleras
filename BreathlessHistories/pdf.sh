6052beed0d2b65d95d49ba93975acb68d8571f10
gwannon <monclus.jorge@gmail.com>
2025-07-30 12:20:30 +0200

    Lanzmaiento ElRio

--- BreathlessHistories/pdf.sh
+++ BreathlessHistories/pdf.sh
@@ -3,11 +3,11 @@
 php ./generateDocs.php ElRio
 chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./temp.pdf ./ElRio.html
 pdftk './temp.pdf' update_info_utf8 './ElRio.txt' output './ElRio.pdf' compress
-rm ./ElRio.html
+#rm ./ElRio.html
 rm ./temp.pdf
 
 php ./generateDocs.php TheRiver
 chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./temp.pdf ./TheRiver.html
 pdftk './temp.pdf' update_info_utf8 './TheRiver.txt' output './TheRiver.pdf' compress
-rm ./TheRiver.html
+#rm ./TheRiver.html
 rm ./temp.pdf
