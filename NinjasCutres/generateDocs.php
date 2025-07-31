97b3abe910ff008bd83260dee1ee4d179ecacbb2
Gwannon <monclus.jorge@gmail.com>
2025-07-31 09:24:32 +0200

    Lanzamiento Ninjas cutres

--- NinjasCutres/generateDocs.php
+++ NinjasCutres/generateDocs.php
@@ -10,7 +10,7 @@ $mkd->setContent(file_get_contents(__DIR__ . "/".$argv[1].".md"));
 
 file_put_contents(__DIR__ . "/Acc".$argv[1].".md", str_replace(["\sp", "\sc", "\sinc", "\conc", "&nbsp;\n", "\n\n\n"], "", file_get_contents(__DIR__ . "/".$argv[1].".md")));
 
-$html = str_replace("|HTML|", $mkd->toHtml(), file_get_contents(__DIR__ . "/template.html")); 
+$html = str_replace("|HTML|", $mkd->toHtml(), file_get_contents(__DIR__ . "/template".$argv[1].".html")); 
 $html = str_replace("|ID|", $argv[1], $html); 
 $html = str_replace("<hr />", "</div><div class=\"saltopagina\"></div>\n</section>\n<section>", $html); 
 $html = str_replace("<p>\sp</p>", "</div><div class=\"saltopagina\"></div><div class='columns'>", $html);
