2c95801cba47a3c032208f05d762e59657a6d5c0
gwannon <monclus.jorge@gmail.com>
2026-03-18 09:22:12 +0100

    Cavernícola traducción

--- CrimenCavernicola/pdf.sh
+++ CrimenCavernicola/pdf.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-php ./generateDocs.php CrimenCavernicola
-chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./temp.pdf ./CrimenCavernicola.html
-pdftk './temp.pdf' update_info_utf8 './CrimenCavernicola.txt' output '../CrimenCavernicola.pdf' compress
-rm ./CrimenCavernicola.html
-rm ./CrimenCavernicola.txt
-rm ./temp.pdf
+#php ./generateDocs.php CrimenCavernicola
+#chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./temp.pdf ./CrimenCavernicola.html
+#pdftk './temp.pdf' update_info_utf8 './CrimenCavernicola.txt' output '../CrimenCavernicola.pdf' compress
+#rm ./CrimenCavernicola.html
+#rm ./CrimenCavernicola.txt
+#rm ./temp.pdf
 
-php ./generateDocs.php CrimenCavernicolaBW
-chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./temp.pdf ./CrimenCavernicolaBW.html
-pdftk './temp.pdf' update_info_utf8 './CrimenCavernicolaBW.txt' output '../CrimenCavernicolaBW.pdf' compress
-rm ./CrimenCavernicolaBW.html
-rm ./CrimenCavernicolaBW.txt
-rm ./AccCrimenCavernicolaBW.md
-rm ./temp.pdf
+#php ./generateDocs.php CrimenCavernicolaBW
+#chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./temp.pdf ./CrimenCavernicolaBW.html
+#pdftk './temp.pdf' update_info_utf8 './CrimenCavernicolaBW.txt' output '../CrimenCavernicolaBW.pdf' compress
+#rm ./CrimenCavernicolaBW.html
+#rm ./CrimenCavernicolaBW.txt
+#rm ./AccCrimenCavernicolaBW.md
+#rm ./temp.pdf
 
 php ./generateDocs.php CavemanCrime
 chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./temp.pdf ./CavemanCrime.html
