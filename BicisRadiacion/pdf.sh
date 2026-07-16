c5bee385d11ad185bce9564a4ce719b476f44e5e
Gwannon <monclus.jorge@gmail.com>
2026-07-17 00:23:17 +0200

    B&R 1.0 traducción

--- BicisRadiacion/pdf.sh
+++ BicisRadiacion/pdf.sh
@@ -6,7 +6,7 @@
 #rm ./BicisRadiacion.html
 #rm ./temp.pdf
 
-##php ./generateDocs.php BicisRadiacionBW
+#php ./generateDocs.php BicisRadiacionBW
 #chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./temp.pdf ./BicisRadiacionBW.html
 #pdftk './temp.pdf' update_info_utf8 './BicisRadiacionBW.txt' output '../BicisRadiacionBW.pdf' compress
 #rm ./BicisRadiacionBW.html
